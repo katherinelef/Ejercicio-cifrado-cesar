0bedf0e9b7c3cf41d8dedb47b6516d664f0b5771
katherinelef <katherine.lef@hotmail.com>
2018-01-26 00:17:24 -0500

    usando eslint

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 // Crear una funcion llamada cipher que cifre el texto ingresado//
-let button = document.getElementById('cifrar')
-button.addEventListener("click", cipher)
+let button = document.getElementById('cifrar');
+button.addEventListener('click', cipher);
 
 function cipher() {
-  let texto = document.getElementById('texto').value
+  let texto = document.getElementById('texto').value;
   let cifrado = '';
   // el for recorrera las letras del texto a cifrar//
   for (let i = 0; i < texto.length; i++) {
     if (parseInt(texto[i]) % 1 === 0) // condicionar para no ingresar numeros//
       texto = prompt('Por favor ingrese un texto sin numeros ni espacios');
-      let ubicacionCesar = (texto.toUpperCase().charCodeAt(i) - 65 + 33) % 26 + 65;
-      let palabraCifrada = String.fromCharCode(ubicacionCesar)
-      cifrado += palabraCifrada; // acumular las letras cifradas//
+    let ubicacionCesar = (texto.toUpperCase().charCodeAt(i) - 65 + 33) % 26 + 65;
+    let palabraCifrada = String.fromCharCode(ubicacionCesar);
+    cifrado += palabraCifrada; // acumular las letras cifradas//
   }
   var newParagraph = document.createElement('p');
   var paragraphText = document.createTextNode(cifrado);
@@ -20,7 +20,7 @@ function cipher() {
 }
 
 let buttonDescipher = document.getElementById('descifrar');
-buttonDescipher.addEventListener("click", descipher)
+buttonDescipher.addEventListener('click', descipher);
 // Crear una funcion llamada decipher que descifre el texto ingresado //
 function descipher() {
   let texto = document.getElementById('texto').value;
